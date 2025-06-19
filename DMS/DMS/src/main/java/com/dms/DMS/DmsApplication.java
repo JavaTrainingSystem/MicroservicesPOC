edfa8b02bbabbc748a8f953393b168dc5456e7a4
Sri Harsha Gangavelli <harshagangavelli17@gmail.com>
2025-06-19 13:57:47 +0530

    Microservices Init

---
+++ DMS/DMS/src/main/java/com/dms/DMS/DmsApplication.java
@@ -0,0 +1,14 @@
+package com.dms.DMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@EnableEurekaCl
+public class DmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmsApplication.class, args);
+	}
+
+}
